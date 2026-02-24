cfe6480392ab72eb7561f4a46098a8c2b5901104
Matthias Moser <moserm763@gmail.com>
2026-02-24 11:20:09 +0100

    removed bunch of outdated files

--- Classifier/src/classes/functions.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def weighted_average(loss_per_sample, weights, eps = 1e-12):
-    denom = weights.sum()
-    denom = torch.clamp(denom, min=eps)
-    return (loss_per_sample * weights).sum()/denom
