8c7829c9beb7ab8a945402b2caf8176c3225d1ca
Tamara App <tamara.app@student.kit.edu>
2026-08-17 19:48:22 +0200

    mask works

--- DNN_tt/src/classes/fraction_factor.py
+++ DNN_tt/src/classes/fraction_factor.py
@@ -4,14 +4,24 @@ import pandas as pd
 
 def pt_mask(df):
     bin_edges = [40, 45, 50 , 55, 60, 65, 70, 75, 80, 90, 100, 120, 200]
-    bins = np.zeros((len(bin_edges), len(bin_edges)))
-    for i in range(len(bin_edges)):
-        mask_pt2 = (bin_edges[i]<=df.pt_2) & (df.pt_2 < bin_edges[i+1])
-        for j in range(len(bin_edges)):
-            mask_pt1 = (bin_edges[j]<=df.pt_1) & (df.pt_1 < bin_edges[j+1])    
-            bins[i][j] = df[mask_pt1 & mask_pt2] 
-    print(np.identity(3))
-    return bins
+    n_bins = len(bin_edges) -1
+
+    masks = np.empty((n_bins, n_bins))
+    for i in range(n_bins):
+        if i == np.max(range(n_bins)):
+            mask_pt2 = ((bin_edges[i] <= df["pt_2"]))
+        else:
+            mask_pt2 = ((bin_edges[i] <= df["pt_2"]) & (df["pt_2"] < bin_edges[i + 1]))        
+
+        for j in range(n_bins):
+            if j == np.max(range(n_bins)):
+                mask_pt1 = ((bin_edges[j] <= df["pt_1"]))
+            else:
+                mask_pt1 = ((bin_edges[j] <= df["pt_1"]) & (df["pt_1"] < bin_edges[j + 1]))
+
+            masks[i, j] = mask_pt1 & mask_pt2
+
+    return masks
 
 def fraction_in_bins(df):
     return None
